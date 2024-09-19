f20a80137694659e539eb437abf31f70cb442ec4
akshit-vashishtha <akshitv322@gmail.com>
2024-09-19 21:14:40 +0530

    added color picker and eraser

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const canvas=document.querySelector("canvas");
 let toolButtons=document.querySelectorAll(".tools")
 let fillColor=document.querySelector("#fill-color");
+let size=document.querySelector("#size-slider");
+let colorBtns=document.querySelectorAll(".colors .option");
+let colorPicker=document.querySelector("#color-picker");
 console.log(toolButtons);
 ctx=canvas.getContext("2d");
 
 let prevMouseX, prevMouseY, snapshot,
 isDrawing=false,
 selectedTool="brush",
-brushSize=5;
+brushSize=5,
+selectedColor="#000";
 
 
 window.addEventListener("load",()=>{
@@ -46,6 +50,8 @@ const startDraw=(e)=>{
     prevMouseY=e.offsetY;
     ctx.beginPath(); //creates a new path to draw if not added it starts from the point we left mouse
     ctx.lineWidth=brushSize;
+    ctx.strokeStyle=selectedColor;
+    ctx.fillStyle=selectedColor;
     snapshot=ctx.getImageData(0, 0, canvas.width, canvas.height);
 }
 
@@ -53,7 +59,9 @@ const drawing = (e) => {
     if(!isDrawing) return;
     ctx.putImageData(snapshot,0,0);
 
-    if(selectedTool==="brush"){
+    if(selectedTool==="brush" || selectedTool==="eraser"){
+        //if selected tool is eraser then strokestyle that is color of brush white which will act like eraser
+        ctx.strokeStyle=selectedTool==="eraser"?"#fff":selectedColor;
         ctx.lineTo(e.offsetX, e.offsetY); // creates line according to mouse pointer
         ctx.stroke(); // filling line with color
     } else if(selectedTool==="rectangle"){
@@ -75,6 +83,20 @@ toolButtons.forEach(btn => {
     });
 });
 
+colorBtns.forEach(btn=>{
+    btn.addEventListener("click",()=>{
+        document.querySelector(".options .selected").classList.remove("selected");
+        btn.classList.add("selected");
+        selectedColor = window.getComputedStyle(btn).getPropertyValue("background-color");
+    })
+})
+
+colorPicker.addEventListener("change", ()=>{
+    colorPicker.parentElement.style.background=colorPicker.value;
+    colorPicker.parentElement.click();
+})
+
 canvas.addEventListener("mousedown", startDraw);
 canvas.addEventListener("mousemove", drawing);
 canvas.addEventListener("mouseup", ()=>{isDrawing=false});
+size.addEventListener("change",()=>{brushSize=size.value});
