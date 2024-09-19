6335f6c9ad6831c2a3a2b42d4169dc7cd7a5fdfe
akshit-vashishtha <akshitv322@gmail.com>
2024-09-19 15:30:14 +0530

    implemented rectangle both filled and hollow

--- script.js
+++ script.js
@@ -1,32 +1,56 @@
 const canvas=document.querySelector("canvas");
 let toolButtons=document.querySelectorAll(".tools")
+let fillColor=document.querySelector("#fill-color");
 console.log(toolButtons);
 ctx=canvas.getContext("2d");
 
-let isDrawing=false;
-let brushSize=5;
+let prevMouseX, prevMouseY, snapshot,
+isDrawing=false,
+selectedTool="brush",
+brushSize=5;
+
+
 window.addEventListener("load",()=>{
     canvas.width=canvas.offsetWidth;
     canvas.height=canvas.offsetHeight;
 });
 
-const startDraw=()=>{
+const drawRectangle = (e) => {
+    if(fillColor.checked){
+        ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+    }
+    else ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+}
+
+const startDraw=(e)=>{
     isDrawing=true;
+    //passing current mouse pos as prev vals
+    prevMouseX=e.offsetX;
+    prevMouseY=e.offsetY;
     ctx.beginPath(); //creates a new path to draw if not added it starts from the point we left mouse
     ctx.lineWidth=brushSize;
+    snapshot=ctx.getImageData(0, 0, canvas.width, canvas.height);
 }
 
 const drawing = (e) => {
     if(!isDrawing) return;
-    ctx.lineTo(e.offsetX, e.offsetY); // creates line according to mouse pointer
-    ctx.stroke(); // filling line with color
+    ctx.putImageData(snapshot,0,0);
+
+    if(selectedTool==="brush"){
+        ctx.lineTo(e.offsetX, e.offsetY); // creates line according to mouse pointer
+        ctx.stroke(); // filling line with color
+    } else if(selectedTool==="rectangle"){
+        drawRectangle(e);
+    }
+    
 }
 
 toolButtons.forEach(btn => {
     btn.addEventListener("click",()=>{
         document.querySelector(".options .active").classList.remove("active");
         btn.classList.add("active");
-        console.log(btn.id);  // Check if btn has the correct element and ID
+        selectedTool= btn.id;
+        console.log(selectedTool);
     });
 });
 
